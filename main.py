b61ae7d003fc0c64503b9c86e8054c3cfb974449
Miguel <miguelherranz717@gmail.com>
2021-08-10 01:47:15 +0200

    Added sanitation checks and some comments

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+# Simple script to download the list of companies 
+# working in Madrid from the audiovisual field.
+
+# Python depencencies
 import pandas as pd
-from os import system
+import subprocess as sp
 
-system('node dump_empresas.js');
+# Start by downloading the "empresas.js" file from the filmmadrid web
+try:
+    print("Downloading empresas.js")
+    node_process = sp.run(["node", "dump_empresas.js"])
+    print("Download successful")
+except PermissionError:
+    print("nodejs not found.\nTry 'sudo apt install nodejs'")
+    exit()
 
+# Parse the downloaded json file as a Dataframe in pandas
 data = pd.read_json("empresas.json")
 
-data.to_excel('empresas.xlsx', index=False)
+# Create Excel file for ease of use
+try:
+    print("Creating Excel file")
+    data.to_excel('empresas.xlsx', index=False)
+    print("Excel created successfuly")
+except:
+    print("Could not create Excel")
